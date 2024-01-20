00679207569d2de4c4f238aed6800d807d86d3c8
Aldrick Rasquinha <aldrickrasquinha@gmail.com>
2024-01-20 19:22:42 +0530

    v1.1

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const volumeEl = document.getElementById("volume-el");
 const massEL = document.getElementById("mass-el");
 const lengthEL = document.getElementById("length-el");
 
-convertBtn.addEventListener("click", function test() {
+convertBtn.addEventListener("click", test)
+
+test()
+
+function test() {
     let inputValue = Number(inputEL.value);
     if (inputValue<0) {
         alert("Enter a positive number")
@@ -13,8 +17,8 @@ convertBtn.addEventListener("click", function test() {
         alert("Enter a number less than 1M")
     }
     else {
-        volumeEl.innerText = `${(inputValue).toFixed(2)} liters = ${(inputValue * 0.264).toFixed(2)} gallons | ${(inputValue).toFixed(2)} gallons = ${(inputValue / 0.264).toFixed(2)} liters`;
-        massEL.innerText = `${(inputValue).toFixed(2)} kilos = ${(inputValue * 2.204).toFixed(2)} pounds | ${(inputValue).toFixed(2)} pounds = ${(inputValue / 2.204).toFixed(2)} kilos`;
-        lengthEL.innerText = `${(inputValue).toFixed(2)} meters = ${(inputValue * 3.281).toFixed(2)} feet | ${(inputValue).toFixed(2)} feet = ${(inputValue / 3.281).toFixed(2)} meters`
+        volumeEl.innerHTML = `<b>${(inputValue).toFixed(2)}</b> liters = <b>${(inputValue * 0.264).toFixed(2)}</b> gallons | <b>${(inputValue).toFixed(2)}</b> gallons = <b>${(inputValue / 0.264).toFixed(2)}</b> liters`;
+        massEL.innerHTML = `<b>${(inputValue).toFixed(2)}</b> kilos = <b>${(inputValue * 2.204).toFixed(2)}</b> pounds | <b>${(inputValue).toFixed(2)}</b> pounds = <b>${(inputValue / 2.204).toFixed(2)}</b> kilos`;
+        lengthEL.innerHTML = `<b>${(inputValue).toFixed(2)}</b> meters = <b>${(inputValue * 3.281).toFixed(2)}</b> feet | <b>${(inputValue).toFixed(2)}</b> feet = <b>${(inputValue / 3.281).toFixed(2)}</b> meters`
     }
-})
+}
